2568bc08afd649516999217c892faa3a165306a7
Eniola <opakunleeniolail2019@gmail.com>
2024-08-02 09:45:15 +0100

    feat: styled the latest articles and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import logo from "../assets/images/logo.svg";
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <div className="bg-[Hex: #e9181a]">
+      <div className="w-4/5 m-auto py-20">
+        <div className="grid grid-cols-4">
+          <div className="flex flex-col">
+            <img src={logo} alt="" className="w-1/2"/>
+          </div>
+          <div className="flex flex-col gap-y-5">
+            <p>About </p>
+            <p>Contact</p>
+            <p>Blog</p>
+          </div>
+          <div className="flex flex-col gap-y-5">
+            <p>Career</p>
+            <p>Support</p>
+            <p>Privacy Policy</p>
+          </div>
+          <div>
+            <button className="bg-black text-white px-7 py-2 rounded-full">
+              Request Invite
+            </button>
+            <p>©Easybank. All Rights Reserved</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,13 @@ import React from "react";
 import bgIntro from "../assets/images/bg-intro-desktop.svg";
 import mockups from "../assets/images/image-mockups.png";
 import onlineIcon from "../assets/images/icon-online.svg";
-import onboardingIcon from '../assets/images/icon-onboarding.svg';
-import budgetingIcon from '../assets/images/icon-budgeting.svg';
-import apiIcon from '../assets/images/icon-api.svg';
-
+import onboardingIcon from "../assets/images/icon-onboarding.svg";
+import budgetingIcon from "../assets/images/icon-budgeting.svg";
+import apiIcon from "../assets/images/icon-api.svg";
+import currencyImage from '../assets/images/image-currency.jpg'
+import restaurantImage from '../assets/images/image-restaurant.jpg'
+import planeImage from '../assets/images/image-plane.jpg'
+import confettiImage from '../assets/images/image-confetti.jpg'
 
 function Home() {
   return (
@@ -34,6 +37,7 @@ function Home() {
           </div>
         </div>
       </div>
+      {/* why choose us? */}
       <div className="bg-gray-100 pb-20">
         <div className=" flex flex-col gap-y-4 w-4/5 m-auto pt-32">
           <div>
@@ -51,29 +55,85 @@ function Home() {
               <img src={onlineIcon} alt="" className="w-1/5" />
               <p className="text-[24px]">Online Banking</p>
               <p>
-                Our web and mobile applications allow you to keep track of your finances wherever you are in the world
+                Our web and mobile applications allow you to keep track of your
+                finances wherever you are in the world
               </p>
             </div>
             <div className="flex flex-col gap-y-4">
               <img src={budgetingIcon} alt="" className="w-1/5" />
               <p className="text-[24px]">Simple Budgeting</p>
               <p>
-                See exactly where your money goes each month. Receive notifications when you're close to hitting your limits
+                See exactly where your money goes each month. Receive
+                notifications when you're close to hitting your limits
               </p>
             </div>
             <div className="flex flex-col gap-y-4">
               <img src={onboardingIcon} alt="" className="w-1/5" />
               <p className="text-[24px]">Fast Onboarding</p>
               <p>
-                We don't do branches. Open your account in minutes online and start taking control of your finances right away.
+                We don't do branches. Open your account in minutes online and
+                start taking control of your finances right away.
               </p>
             </div>
             <div className="flex flex-col gap-y-4">
               <img src={apiIcon} alt="" className="w-1/5" />
               <p className="text-[24px]">Open Api</p>
               <p>
-                Manage your savings, investments, pension, and much more from one account. Tracking your money has never been easier.
+                Manage your savings, investments, pension, and much more from
+                one account. Tracking your money has never been easier.
               </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* latest articles */}
+      <div className="bg-gray-50">
+        <div className="w-4/5 m-auto py-20 flex flex-col gap-y-10">
+          <p className="text-[40px]">Latest Articles</p>
+          <div className="grid grid-cols-4 gap-x-10">
+            <div className="rounded-lg bg-white">
+              <img src={currencyImage} alt="" className="rounded-t-lg h-[200px]" />
+              <div className="p-7 pr-10 flex flex-col gap-y-3">
+                <p className="text-[12px] text-gray-400">By Claire Robinson</p>
+                <p className="text-[20px] leading-tight">Receive money in any currency with no fees</p>
+                <p className="text-[14px] text-gray-500">
+                  The world is getting smaller and we're becoming mobile. So why
+                  should you be forced to only receive money in a single...
+                </p>
+              </div>
+            </div>
+            <div className="rounded-lg bg-white">
+              <img src={restaurantImage} alt="" className="rounded-t-lg h-[200px]" />
+              <div className="p-7 pr-10 flex flex-col gap-y-3">
+                <p className="text-[12px] text-gray-400">By Claire Robinson</p>
+                <p className="text-[20px] leading-tight">Receive money in any currency with no fees</p>
+                <p className="text-[14px] text-gray-500">
+                  The world is getting smaller and we're becoming mobile. So why
+                  should you be forced to only receive money in a single...
+                </p>
+              </div>
+            </div>
+            <div className="rounded-lg bg-white">
+              <img src={planeImage} alt="" className="rounded-t-lg h-[200px]" />
+              <div className="p-7 pr-10 flex flex-col gap-y-3">
+                <p className="text-[12px] text-gray-400">By Claire Robinson</p>
+                <p className="text-[20px] leading-tight">Receive money in any currency with no fees</p>
+                <p className="text-[14px] text-gray-500">
+                  The world is getting smaller and we're becoming mobile. So why
+                  should you be forced to only receive money in a single...
+                </p>
+              </div>
+            </div>
+            <div className="rounded-lg bg-white">
+              <img src={confettiImage} alt="" className="rounded-t-lg h-[200px]" />
+              <div className="p-7 pr-10 flex flex-col gap-y-3">
+                <p className="text-[12px] text-gray-400">By Claire Robinson</p>
+                <p className="text-[20px] leading-tight">Receive money in any currency with no fees</p>
+                <p className="text-[14px] text-gray-500">
+                  The world is getting smaller and we're becoming mobile. So why
+                  should you be forced to only receive money in a single...
+                </p>
+              </div>
             </div>
           </div>
         </div>
